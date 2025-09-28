abbfc1ae12473db59c11ef0a3e7e8c9ff1af3994
Mamedova Kubra <kiubrajm@code.edu.az>
2025-09-28 17:41:14 +0400

    added header and footer

---
+++ app/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+
+function App() {
+  
+
+  return (
+    <>
+     <Header/>
+     app
+     <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ app/src/components/Header/Header.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
+import "../Header/Header.css";
+
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+
+  return (
+    <header>
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+
+      <div className={`navbar ${isOpen ? "open" : ""}`}>
+        <nav>
+          <NavLink to="/" end onClick={() => setIsOpen(false)}>Home</NavLink>
+          <NavLink to="/shop" onClick={() => setIsOpen(false)}>Shop</NavLink>
+          <NavLink to="/about" onClick={() => setIsOpen(false)}>About</NavLink>
+          <NavLink to="/contact" onClick={() => setIsOpen(false)}>Contact</NavLink>
+          <NavLink to="/blog" onClick={() => setIsOpen(false)}>Blog</NavLink>
+        </nav>
+      </div>
+
+      <div className="account">
+        <NavLink to="/account">My Account</NavLink>
+      </div>
+
+      <div className={`burger ${isOpen ? "toggle" : ""}`} onClick={() => setIsOpen(!isOpen)}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
